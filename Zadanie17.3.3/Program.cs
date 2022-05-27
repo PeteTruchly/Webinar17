9c3ae94106345d03acbd28d5cc479bed17206815
Peter Trúchly <peter92206@gmail.com>
2022-05-27 20:48:17 +0200

    Add project files.

---
+++ Zadanie17.3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+int number;
+Console.Write("Guess the number from 1 to 10: ");
+number = Convert.ToInt32(Console.ReadLine());
+Random random = new Random();
+var randomNumber = random.Next(1, 11);
+
+
+Console.WriteLine($"Your number is: {number}");
+if(number == randomNumber)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Your guess is correct :)");
+    Console.ResetColor();
+} else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Your quess is not correct! Random number is: {randomNumber}");
+    Console.ResetColor();
+}
+Console.ReadKey();
